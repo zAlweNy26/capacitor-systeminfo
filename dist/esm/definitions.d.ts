38f5171332a4f71fdf088349ee9f054dff197499
zAlweNy26 <zAlweNy26@users.noreply.github.com>
2023-09-08 23:45:01 +0000

    Build package

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -1,18 +1,40 @@
 import type { PluginListenerHandle } from '@capacitor/core';
-export interface RunTimeInfos {
-    ram: number;
-    hdd: number;
+export interface RuntimeInfos {
+    usedRAM?: number;
+    usedHDD?: number;
+    usedSD?: number;
 }
-export interface SoftWareInfos {
-    sdkVersion: number;
+export interface SoftwareInfos {
+    osName: string;
+    osVersion: string;
+    brandName: string;
+    sdkVersion?: number;
+    sdkName?: string;
+    securityPatch?: string;
+    uiVersion?: string;
+    deviceID?: string;
+    boardName?: string;
+    bootloaderVersion?: string;
+    supportedABIs?: string[];
 }
-export interface HardWareInfos {
-    cpu: number;
+export type Features = 'bluetooth' | 'bluetoothLowEnergy' | 'microphone' | 'speaker' | 'nfc' | 'camera' | 'gamepad' | 'gps' | 'touchscreen' | 'wifi' | 'fingerprint' | 'face' | 'ethernet';
+export interface HardwareInfos {
+    modelID?: string;
+    modelCodeName?: string;
+    cpuModel?: string;
+    cpuCores?: [number, number][];
+    totalSD?: number;
+    totalHDD: number;
+    totalRAM: number;
+    totalCores: number;
+    manufacturer: string;
+    features: Features[];
 }
-export type SystemInformations = SoftWareInfos & HardWareInfos & RunTimeInfos;
+export type SystemInformations = SoftwareInfos & HardwareInfos & RuntimeInfos;
 export interface SystemInfoPlugin {
-    start(): Promise<SystemInformations | undefined>;
+    getInfos(): Promise<SystemInformations>;
+    start(): Promise<void>;
     stop(): Promise<void>;
-    addListener(eventName: 'runTimeChange', listenerFunc: (event: RunTimeInfos) => void): Promise<PluginListenerHandle>;
+    addListener(eventName: 'runtimeChange', listenerFunc: (event: RuntimeInfos) => void): Promise<PluginListenerHandle>;
     removeAllListeners(): Promise<void>;
 }
--- dist/esm/web.d.ts
+++ dist/esm/web.d.ts
@@ -1,6 +1,10 @@
+import type { ListenerCallback, PluginListenerHandle } from '@capacitor/core';
 import { WebPlugin } from '@capacitor/core';
-import type { SystemInfoPlugin, SystemInformations } from './definitions';
+import { SystemInformations } from './definitions';
+import type { SystemInfoPlugin } from './definitions';
 export declare class SystemInfoWeb extends WebPlugin implements SystemInfoPlugin {
-    start(): Promise<SystemInformations | undefined>;
+    getInfos(): Promise<SystemInformations>;
+    start(): Promise<void>;
     stop(): Promise<void>;
+    addListener(eventName: string, listenerFunc: ListenerCallback): Promise<PluginListenerHandle> & PluginListenerHandle;
 }
