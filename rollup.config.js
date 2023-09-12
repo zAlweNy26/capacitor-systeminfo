cb8d4d161fe6e1a549d2abac35aeb05ccffa12a8
Dany <zdanymc@gmail.com>
2023-09-12 19:41:06 +0200

    Create docs.json

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ export default {
       name: 'capacitorSystemInfo',
       globals: {
         '@capacitor/core': 'capacitorExports',
+        'platform': 'platformExports'
       },
       sourcemap: true,
       inlineDynamicImports: true,
